dfd9ee332a24a873d37e81b6b87c5fd9f82988b6
Alice-Retz <alicegretz@gmail.com>
2021-11-29 16:48:54 -0600

    State variables formatted in Home.js

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Preview from '../../components/Preview/Preview'
 import Editor from '../../components/Editor/Editor'
@@ -7,12 +7,39 @@ import './Home.css'
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('')
+  const [subtitle, setSubtitle] = useState('')
+  const [font, setFont] = useState('')
+  const [align, setAlign] = useState('')
+  const [text, setText] = useState('')
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview
+        title={title}
+        setTitle={setTitle}
+        subtitle={subtitle}
+        setSubtitle={setSubtitle}
+        font={font}
+        setFont={setFont}
+        align={align}
+        setAlign={setAlign}
+        text={text}
+        setText={setText}
+      />
+      <Editor
+        title={title}
+        onTitleChange={setTitle}
+        subtitle={subtitle}
+        onSubtitleChange={setSubtitle}
+        font={font}
+        onFontChange={setFont}
+        align={align}
+        onAlignChange={setAlign}
+        text={text}
+        onTextChange={setText}
+      />
     </main>
   )
 }
