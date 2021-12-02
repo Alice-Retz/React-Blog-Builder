ba5b41b6c9bee6edda2e9fe1b43bbeaa068a3be6
Alice-Retz <alicegretz@gmail.com>
2021-12-01 20:01:24 -0600

    Further tweaking of the prettierrc, eslintrc, and package.json

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import Preview from '../../components/Preview/Preview'
-import Editor from '../../components/Editor/Editor'
+import Preview from '../../components/Preview/Preview';
+import Editor from '../../components/Editor/Editor';
 
-import './Home.css'
+import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-  const [title, setTitle] = useState('')
-  const [subtitle, setSubtitle] = useState('')
-  const [font, setFont] = useState('')
-  const [align, setAlign] = useState('')
-  const [text, setText] = useState('')
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [font, setFont] = useState('');
+  const [align, setAlign] = useState('');
+  const [text, setText] = useState('');
 
   return (
     <main>
@@ -31,5 +31,5 @@ export default function Home() {
         setText={setText}
       />
     </main>
-  )
+  );
 }
